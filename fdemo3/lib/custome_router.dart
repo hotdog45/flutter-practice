b06d090dff67ea031d8185237a135d523af8235f
李顺风 <mac2020@126.com>
2019-03-20 10:42:50 +0800

    2

--- fdemo3/lib/custome_router.dart
+++ fdemo3/lib/custome_router.dart
@@ -5,7 +5,7 @@ class CustomRoute extends PageRouteBuilder {
 
   CustomRoute(this.widget)
       : super(
-            transitionDuration: const Duration(seconds: 3),
+            transitionDuration: const Duration(seconds: 1),
             pageBuilder: (BuildContext context, Animation<double> animation1,
                 Animation<double> animation2) {
               return widget;
@@ -14,10 +14,37 @@ class CustomRoute extends PageRouteBuilder {
                 Animation<double> animation1,
                 Animation<double> animation2,
                 Widget child) {
-              return FadeTransition(
-                opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-                    parent: animation1, curve: Curves.fastOutSlowIn)),
+              //渐隐渐现
+              // return FadeTransition(
+              //   opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //       parent: animation1, curve: Curves.fastOutSlowIn)),
+              //   child: child,
+              // );
+
+              //缩放
+              // return ScaleTransition(
+              //   child: child,
+              //   scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //       parent: animation1, curve: Curves.fastOutSlowIn)),
+              // );
+
+              //旋转 缩放
+              // return RotationTransition(
+              //     turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //         parent: animation1, curve: Curves.fastOutSlowIn)),
+              //     child: ScaleTransition(
+              //       child: child,
+              //       scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //           parent: animation1, curve: Curves.fastOutSlowIn)),
+              //     ));
+
+              //左右滑动路由
+              return SlideTransition(
                 child: child,
+                position: Tween<Offset>(
+                        begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0))
+                    .animate(CurvedAnimation(
+                        parent: animation1, curve: Curves.decelerate)),
               );
             });
 }
